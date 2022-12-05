2aa1db68d23b474e8273e558276e006738617f91
nikitaj-57 <nikitajit2@gmail.com>
2022-12-05 21:44:03 +0530

    Assignment-6 code files

---
+++ assignment-6/src/Components/index.js
@@ -0,0 +1,5 @@
+import CardComponent from "./CardContainer";
+import TitleComponent from "./TitleComponent";
+import SearchBar from "./SearchBar";
+
+export { CardComponent, TitleComponent, SearchBar };
